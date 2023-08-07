2e340f68f1ec065f11f1625926171f6a138f6694
cedric karungu <ckarungu921@gmail.com>
2023-08-07 17:25:41 +0200

    feat: useState hook

--- 01_x_react17/pages/index.js
+++ 01_x_react17/pages/index.js
@@ -1,8 +1,27 @@
 import React from 'react';
 
 const InputElement = () => {
+	const [inputText, setInputText] = React.useState('');
+	const [historyList, setHistoryList] = React.useState([]);
 	return (
-		<input onChange={(e) => console.log(e)} placeholder='Enter some text' />
+		<div>
+			<input
+				onChange={(e) => {
+					setInputText(e.target.value);
+					setHistoryList((p) => [...p, e.target.value]);
+				}}
+				placeholder='Enter some text'
+			/>
+			<br />
+			{inputText}
+			<hr />
+			<br />
+			<ul>
+				{historyList.map((letter) => {
+					return <li>{letter}</li>;
+				})}
+			</ul>
+		</div>
 	);
 };
 
