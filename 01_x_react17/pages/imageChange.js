2937653baa958191c0fb5f3533050548316bc227
cedric karungu <ckarungu921@gmail.com>
2023-08-08 16:25:22 +0200

    hooks

---
+++ 01_x_react17/pages/imageChange.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ImageChange = () => {
+	return (
+		<div>
+			<img src='/images/boat1.jpg' alt='' />
+			<img src='/images/boat2.jpg' alt='' />
+		</div>
+	);
+};
+
+export default ImageChange;
